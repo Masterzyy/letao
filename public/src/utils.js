90c72734a8aa7c28ed551c849de9ad4f72175901
masterzyy <18663885731@163.com>
2017-11-05 20:03:01 +0800

    实现验证管理员登录和退出功能（AMD模块化）

---
+++ public/src/utils.js
@@ -0,0 +1,25 @@
+define([
+    'jquery'
+], function($) {
+    $.ajax({
+        url:'/api/employee/checkRootLogin',
+        type:'get',
+        success:function(info){
+            if(info.error){
+                location.href='/login.html';
+            }
+        }
+    })
+
+    $('.logout').on('click',function(){
+        $.ajax({
+            url:'/api/employee/employeeLogout',
+            type:'get',
+            success:function(info){
+                if(info.success){
+                    location.href='/login.html'
+                }
+            }
+        })
+    })
+});
